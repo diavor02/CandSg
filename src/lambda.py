a2966dc9324a2b307066c2615f4c4341cfe9c04c
diavor02 <darius.iavorschi@gmail.com>
2025-03-18 22:19:29 -0400

    test5

--- src/lambda.py
+++ src/lambda.py
@@ -1,9 +1,74 @@
+import re
+import json
 import pandas as pd
 import smtplib
 import ssl
 from email.message import EmailMessage
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
 
-EMAILS= ["darius.iavorschi@gmail.com", "robertcosta378@gmail.com"]
+
+EMAILS= ["darius.iavorschi@gmail.com", "robertcosta378@gmail.com", "burkettj2486@gmail.com"]
+
+URI = "mongodb+srv://dariusiavorschi:rge3zdZplVgaDM5j@cluster0.i5iph.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+CLIENT = MongoClient(URI, server_api=ServerApi('1'))
+
+try:
+    CLIENT.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
+
+
+def extract_key(key_val, body_content):
+    pattern = fr"{key_val}:(.*?),"
+
+    match = re.search(pattern, body_content)
+
+    if match:
+        key = match.group(1)
+    else:
+        key = "Not found"
+
+    return key
+
+def parse_event(input_string):
+    pattern = r"'body': '{(.*?)}'"
+    
+    match = re.search(pattern, input_string)
+    
+    try:
+
+        body_content = match.group(1)
+
+        body_content += ','
+
+        signal = extract_key("Signal", body_content)
+        time = extract_key("Time", body_content)
+        ticker = extract_key("Ticker", body_content)
+        price = extract_key("Price", body_content)
+
+        return {"Signal": signal, 
+                "Time": time,
+                "Ticker": ticker,
+                "Price": price}
+
+    except Exception as e:
+        print(f"Error: {e}")
+
+def input_doc_to_MongoDB(document):
+
+    db = CLIENT['db1'] 
+    collection = db['cl1']  
+
+    document = parse_event(document)
+
+    result = collection.insert_one(document)
+
+    print(f"Document inserted with ID: {result.inserted_id}")
+
+
 
 def send_email(event, email: str) -> None:
     email_sender = 'sendstockupdate@gmail.com'
@@ -29,9 +94,11 @@ def send_email(event, email: str) -> None:
 
 def lambda_handler(event, context) -> dict:
 
+    input_doc_to_MongoDB(parse_event(event))
+
     try:
         for email in EMAILS:
-            send_email(event, email)
+            send_email(str(parse_event(event)), email)
 
         return {"statusCode": 200, "body": "Notification sent."}
 
